b0d99d404c1393b0bf01298abb3d72f7f637290f
Christophe Mertens <christophe.mertens@ulb.be>
2023-10-17 11:45:20 +0200

    HelloWorld

---
+++ lib.php
@@ -0,0 +1,17 @@
+<?php
+
+// WebService for UNILU Enrolments
+// Christophe Mertens (ULB) : christophe.mertens@ulb.be
+
+defined('MOODLE_INTERNAL') || die();
+
+class enrol_unilu_plugin extends enrol_plugin {
+    //private $logfile = false;
+
+    //public function allow_enrol(stdClass $instance) { return true; }
+    //public function allow_unenrol(stdClass $instance) { return true; }
+    //public function allow_manage(stdClass $instance) { return true; }
+
+
+
+}
