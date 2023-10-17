77423dd45fe9bf5aef9a6005a4234a8f3e4e0775
Christophe Mertens <christophe.mertens@ulb.be>
2023-10-17 11:37:33 +0200

    HelloWorld

--- settings.php
+++ settings.php
@@ -8,15 +8,14 @@ $hidden = $settings->hidden;
 
 $settings = new admin_category('enroluniluroot', get_string('pluginname', 'enrol_unilu'), $hidden);
 
-
+*/
 if ($ADMIN->fulltree) {
 
     $settings->add(new admin_setting_heading('enrol_unilu_settings', '', get_string('pluginname_descr', 'enrol_unilu')));
 
     //--- course settings -----------------------------------------------------------------------------------
     $settings->add(new admin_setting_heading('enrol_unilu_coursesettings', get_string('course_settings', 'enrol_unilu'), ''));
-    $settingspage->add(new admin_setting_configtext('enrol_unilu/academicyear', get_string('academicyear', 'enrol_unilu'),
-        get_string('academicyearhelp', 'enrol_unilu'), '', '/^[0-9]{6}$/'));
+    //$settingspage->add(new admin_setting_configtext('enrol_unilu/academicyear', get_string('academicyear', 'enrol_unilu'),
+    //    get_string('academicyearhelp', 'enrol_unilu'), '', '/^[0-9]{6}$/'));
 }
 
-*/
